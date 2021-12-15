1579374ea26eedbb1182ab66e69cd9d050717ed9
Cody Grogan <cod.grogan@gmail.com>
2021-12-15 22:02:07 +0800

    interface for create page, better style

--- src/components/Create.js
+++ src/components/Create.js
@@ -5,6 +5,15 @@ function Create(props){
         <div>
             <Navbar/>
             <p>This is Create</p>
+
+
+            <p>In this section, write your own mad lib template</p>
+
+            <p>When you write your story, request words by putting them in parenthesis.</p>
+            <p>ex: Harold was a (adjective) (animal). He loves to (verb).</p>
+            <textarea cols='50' rows="10"></textarea>
+            <br/>
+            <button type="button" className="btn btn-primary">Submit</button>
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,25 @@ function Navbar(props){
         <div>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-                    <Link class="navbar-brand" to='/'>Home</Link>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            
+
+                    <Link className="navbar-brand" to='/'>Home</Link>
                  
-                    <Link class="navbar-brand" to='/play'>Play</Link>
+                    <Link className="navbar-brand" to='/play'>Play</Link>
               
-                    <Link class="navbar-brand" to='/create'>Create</Link>   
-                     
+                    <Link className="navbar-brand" to='/create'>Create</Link>   
+                    
+                    <Link className="navbar-brand" to='/home'>Profile</Link>   
+                    
+                    <button type="button" className='navbar-brand btn btn-outline-primary'  id="sign-in" onclick="signIn()" >
+                    <i className="material-icons">account_circle</i>Sign-in with Google
+                  </button>
                   
+            </div>
             </nav>
         </div>
     )

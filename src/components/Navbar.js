ad8bd80d6466344d62aeac3cff4944de9d0a5154
Cody Grogan <cod.grogan@gmail.com>
2021-12-19 19:19:29 +0800

    profile page layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,6 +82,7 @@ onAuthStateChanged(getAuth(), navauthStateObserver);
                         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
+            <h1>MadlibWarehouse</h1>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
             
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,12 +6,61 @@ import {
   signInWithPopup,
   signOut,
 } from 'firebase/auth';
+import {useState, useEffect} from 'react';
+
 
 function Profile(props){
+
+
+        const [userName, setUserName] = useState("User Name");
+        function getUserName() {
+            return getAuth().currentUser.displayName;
+        
+        }
+        
+        function isUserSignedIn() {
+            return !!getAuth().currentUser;
+        
+        }
+        
+        function authObserver(user){
+            if (user){
+                let name = getAuth().currentUser.displayName;
+                setUserName(name);
+                
+            }
+        }
+        onAuthStateChanged(getAuth(), authObserver);
+
     return(
         <div>
             <Navbar/>
-            Profile Page Under Construction
+            <div className="container">
+                <div className="row">
+                    <div className="card col-">
+                    
+                    <h2 className='card-header' id='userNameTitle'>{userName}</h2>
+                            <div class="card-body">
+                                <p>Total Madlibs Created: </p>
+                                <p>Your Madlib's total plays: </p>
+                            </div>
+
+                    Profile Page Under Construction
+
+                    </div>
+
+                    <table className="table col-md">
+                                <tr>
+                                <th>Title</th>
+                                <th>Plays</th>
+                                <th>Delete</th>
+                                <th></th>
+                            </tr>
+
+                    </table>
+                </div>        
+
+            </div>
         </div>
     )
 }

f884111beeef0320a65aabc1f700bbb434cb524d
Cody Grogan <cod.grogan@gmail.com>
2021-12-21 16:21:18 +0800

    home page stylized

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,10 @@
 import react from 'react';
 import Navbar from './Navbar';
+import playimg from './img/play.PNG';
+import createimg from './img/create.png';
+import profileimg from './img/profile.PNG';
+import shareimg from './img/share.PNG'
+
 
 function Home(props){
 
@@ -7,8 +12,40 @@ function Home(props){
 
         <div>
             <Navbar/>
+        <div className='homeCon'>
             <h2>Welcome to Madlib Warehouse</h2>
       <p>Play, Create, and Share Madlibs</p>
+
+
+    <div id='carouselContainer' className='carouselCon'>
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100"  src={playimg} alt="First slide"/>
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src={createimg} alt="Second slide"/>
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src={shareimg} alt="Third slide"/>
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src={profileimg} alt="Fourth slide"/>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            </div>
+
+    </div>
+
+    </div>
         </div>
 
     )

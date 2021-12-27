db92f226663f44c414e749317282fd0f2e18c724
Cody Grogan <cod.grogan@gmail.com>
2021-12-27 17:07:58 +0800

    fixed typo in test

--- tests/Create.test.js
+++ tests/Create.test.js
@@ -12,11 +12,11 @@ test('Check if uneven () in input will throw error2', () => {
 
 test('Check if no title will throw erro2', () => {
     expect(parseStory('Hello (world)','', "12341234",'Cody')).toBe('error2');
-  });
+});
 
 test('check object properties title is Hello World' , () => {
     let story = parseStory('Hello (world)','Hello World', "12341234",'Cody');
-    expect(story).toMatchObject({title: "Hello World", name: 'Cody'})
+    expect(story).toMatchObject({title: "Hello World", name: 'Cody'});
 
-    });
+});
 

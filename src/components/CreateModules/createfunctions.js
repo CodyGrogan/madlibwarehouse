eba49767e3a9465f91c9b33748399c987e4e2db0
Cody Grogan <cod.grogan@gmail.com>
2021-12-27 14:22:01 +0800

    refactored create code, added tests

---
+++ src/components/CreateModules/createfunctions.js
@@ -0,0 +1,84 @@
+
+  function parseStory(input, title, uid, name){
+      
+    let parenthesesArray1 = [];
+    let parenthesesArray2 = []
+    let wordArray = [];
+    
+
+    for (let i = 0; i < input.length; i++){
+        //Check for #, since it is used in the Play component to identity where to insert words
+        let letter = input.charAt(i);
+        if (letter == '#'){
+            return 'error1';
+        }
+
+        //saves the index of parenthesis brackets
+       else if (letter == '('){
+            parenthesesArray1.push(i);
+        }
+        else if (letter == ')'){
+            parenthesesArray2.push(i);
+        }
+
+
+    }
+
+    if (parenthesesArray1.length != parenthesesArray2.length || parenthesesArray2.length == 0 || title.length < 1){
+        //returns an error if there are unequal number of left and right parentheses or no parentheses
+        return 'error2';
+
+        
+    }
+    else{
+ 
+    for (let i = 0; i < parenthesesArray1.length; i++){
+
+        let index = parenthesesArray1[i];
+        //the second index number needs to be higher by one to include ')'
+        let secondindex = parenthesesArray2[i] + 1;
+        let removed = input.slice(index, secondindex);
+        wordArray.push(removed);
+        
+        console.log(removed)
+        console.log(wordArray);
+      
+
+
+        
+
+    }
+
+    for (let i = 0; i < wordArray.length; i++){
+
+        let hashstring = '#' + i;
+        input = input.replace(wordArray[i], hashstring);
+
+        //later when reading a madlib from the database, the words
+        //given by the player will be inserted at the index of each #
+    }
+
+    // get the indecies of each ( and )
+
+
+        console.log(parenthesesArray1);
+        console.log(input);
+
+        let madlib = {
+            wordList: wordArray,
+            story: input,
+            title: title,
+            uid: uid,
+            name: name,
+            plays: 0
+
+        }
+
+
+        return madlib;
+
+     }
+
+  }
+
+  module.exports = parseStory;
--- tests/Create.test.js
+++ tests/Create.test.js
@@ -1,5 +1,17 @@
-import {Create, parseStoryFunction} from "../src/components/Create";
+
+const parseStory = require("../src/components/CreateModules/createfunctions")
 
 test('Check if # in input will throw error1', () => {
-    expect(parseStoryFunction('#','TitleTest', "12341234",'Cody')).toBe('error1');
+    expect(parseStory('#','TitleTest', "12341234",'Cody')).toBe('error1');
+  });
+  test('Check if uneven () in input will throw error2', () => {
+    expect(parseStory('Hello (()','TitleTest', "12341234",'Cody')).toBe('error2');
+  });
+
+  test('Check if no title will throw erro2', () => {
+    expect(parseStory('Hello (world)','', "12341234",'Cody')).toBe('error2');
+  });
+
+  test('Check if null title will throw erro2', () => {
+    expect(parseStory('Hello (world)', null, "12341234",'Cody')).toBe('error2');
   });

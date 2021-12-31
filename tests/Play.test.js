41e9a084c21bcc70dbcabe1c6e98c55da7a82772
Cody Grogan <cod.grogan@gmail.com>
2021-12-31 16:07:34 +0800

    added unit test for reverse sort, fixed alphabetsort unittest bug

--- tests/Play.test.js
+++ tests/Play.test.js
@@ -1,6 +1,7 @@
 const playmodules = require('../src/components/PlayModules/PlayModules');
 const sortByPlays = playmodules.sortByPlays;
 const sortByAlphabet = playmodules.sortByAlphabet;
+const reverseSort = playmodules.reverseSort;
 
 
 let testarray = [{plays: 0}, {plays: 4}]
@@ -21,6 +22,19 @@ test('Check if testarray [1].plays is 0 ', () => {
   test('Check if testarray [0].name is A', () => {
 
     let testarray = [{name: 'B'}, {name: 'A'}];
-    let aftertest = sortByPlays(testarray);
-    expect(aftertest[1].name).toBe('A');
+    let aftertest = sortByAlphabet(testarray, 'name');
+    expect(aftertest[0].name).toBe('A');
+  });
+
+  test('reverseSort of [1,2,3] is 3 at index 0', () => {
+
+    let testarray = [1,2,3];
+    testarray = reverseSort(testarray);
+    expect(testarray[0]).toBe(3);
+  });
+  test('reverseSort of [a,b,c] is c at index0', () => {
+
+    let testarray = ['a','b','c'];
+    testarray = reverseSort(testarray);
+    expect(testarray[0]).toBe('c');
   });
